ac4a64003a8c16479573cd9500a1234f00f3e88a
Palak013 <102866830+Palak013@users.noreply.github.com>
2024-09-28 09:59:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './MyComponents/Header';
+import Footer from './MyComponents/Footer';
+import Todos from './MyComponents/Todos';
+function App() {
+  return (
+    <>
+ <Header title="TODOSLIST" searchBar={true}/>
+   <Todos/>
+   <Footer/> 
+   
+    
+    
+    
+    </>
+  );
+}
+
+export default App;
